2be1d2b352983503dc83bf0dc5ec3ec406af3a99
piyush-github12 <piyushgithubaccouunt@gmail.com>
2024-12-25 13:30:21 +0530

    navbar changes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { CiMail } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
@@ -6,12 +5,21 @@ import { IoEarthOutline } from "react-icons/io5";
 import { FaCaretDown } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { IoMenu, IoClose } from "react-icons/io5";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="w-screen overflow-hidden text-xl">
       <div className="bg-[var(--primary-bg-color)] p-4 px-[3vw] w-screen">
-        <div className=" mx-auto flex justify-between items-center max-lg:justify-end">
+        <div className="mx-auto flex justify-between items-center">
           <div className="flex max-lg:hidden gap-4 items-center">
             <div className="flex items-center">
               <CiMail className="w-5 h-5 text-white" />
@@ -28,7 +36,7 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="Search Product"
-                className="ml-2 max-sm:w-[120px]  outline-none bg-[rgb(38,114,190)] text-white placeholder:text-[#ffffffe5] "
+                className="ml-2 max-sm:w-[120px] outline-none bg-[rgb(38,114,190)] text-white placeholder:text-[#ffffffe5]"
               />
             </div>
             <div className="flex items-center ml-4">
@@ -40,17 +48,19 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="bg-white p-4 px-[3vw] w-screen">
-        <div className=" mx-auto flex justify-between items-center">
+      <div className="bg-white p-4 px-[3vw] w-screen relative">
+        <div className="mx-auto flex justify-between items-center">
           <Link to="/" className="flex items-center">
             <img
-              src="/images/navbar/logo.svg"
+              src={logo}
               alt="Logo"
               className="h-[80px] w-[200px] object-contain"
             />
           </Link>
-          <div className="flex max-xl:hidden gap-[4vw] px-[30px] items-center">
-            <Link to="/" className="text-black ">
+
+          {/* Desktop Menu */}
+          <div className="hidden xl:flex gap-[4vw] px-[30px] items-center">
+            <Link to="/" className="text-black">
               Home
             </Link>
             <Link
@@ -60,7 +70,10 @@ const Navbar = () => {
               Solutions
               <IoIosArrowDown className="w-4 h-4 text-black" />
             </Link>
-            <Link to="/allproducts" className="text-black flex items-center gap-1">
+            <Link
+              to="/allproducts"
+              className="text-black flex items-center gap-1"
+            >
               Products
               <IoIosArrowDown className="w-4 h-4 text-black" />
             </Link>
@@ -73,12 +86,72 @@ const Navbar = () => {
               About Us
             </Link>
           </div>
+
+          {/* Hamburger Menu Button */}
+          <div className="xl:hidden flex items-center gap-4">
+            <button onClick={toggleMenu} className="text-black p-2 z-50">
+              {isMenuOpen ? (
+                <IoClose className="w-8 h-8" />
+              ) : (
+                <IoMenu className="w-8 h-8" />
+              )}
+            </button>
+          </div>
+
           <Link
             to="/contactus"
-            className="bg-[var(--primary-bg-color)] text-white px-4 py-2 rounded ml-8"
+            className="bg-[var(--primary-bg-color)] text-white px-4 py-2 rounded ml-8 max-sm:hidden"
           >
             Contact us
           </Link>
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={`xl:hidden fixed top-[132px] left-0 w-full bg-white shadow-lg transition-all duration-300 ease-in-out transform ${
+            isMenuOpen
+              ? "translate-y-0 opacity-100 visible"
+              : "translate-y-[-100%] opacity-0 invisible"
+          } z-40`}
+        >
+          <div className="flex flex-col p-4">
+            <Link to="/" className="py-2 text-black hover:bg-gray-100 px-4">
+              Home
+            </Link>
+            <Link
+              to="/solutions"
+              className="py-2 text-black hover:bg-gray-100 px-4 flex items-center justify-between"
+            >
+              Solutions
+              <IoIosArrowDown className="w-4 h-4 text-black" />
+            </Link>
+            <Link
+              to="/allproducts"
+              className="py-2 text-black hover:bg-gray-100 px-4 flex items-center justify-between"
+            >
+              Products
+              <IoIosArrowDown className="w-4 h-4 text-black" />
+            </Link>
+            <div className="py-2 text-black hover:bg-gray-100 px-4 flex items-center justify-between">
+              Corporates
+              <IoIosArrowDown className="w-4 h-4 text-black" />
+            </div>
+            <div className="py-2 text-black hover:bg-gray-100 px-4">
+              Insights
+            </div>
+            <Link
+              to="/aboutus"
+              className="py-2 text-black hover:bg-gray-100 px-4"
+            >
+              About Us
+            </Link>
+            <Link
+              to="/contactus"
+              className="sm:hidden py-2 text-black hover:bg-gray-100 px-4"
+            >
+              Contact us
+            </Link>
+          </div>
         </div>
       </div>
     </div>
